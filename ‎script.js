2da1ddf20f093f939f0cbd62df7aace05bf4a1e4
Bohdan Voitov <bohdan.voiyov@gmail.com>
2024-10-10 18:29:55 +0300

    fix_html

---
+++ ‎script.js
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////////////////////////
+console.log("=== Завдання 1 ===");
+
+// for
+console.log("Цикл for:");
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+// while
+console.log("Цикл while:");
+let i = 1;
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
+
