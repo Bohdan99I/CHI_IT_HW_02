70bcbee3c5921d87f6778d5d57c9328fc5d8d620
Bohdan Voitov <bohdan.voiyov@gmail.com>
2024-10-10 20:48:15 +0300

    done_task2

--- ‎script.js
+++ ‎script.js
@@ -16,18 +16,36 @@ function addParamsToRequest(params) {
   };
 }
 
-// Використання функції
-console.log("=== Завдання: Функції вищого порядку та замикання ===");
-
-// Створюємо функцію sendData, передаючи об'єкт з access-token
+// sendData, передаємо об'єкт з access-token
 const sendData = addParamsToRequest({ "access-token": "qwerty" });
 
-// Виклик функції sendData з різними даними
+//sendData з різними даними
 const result1 = sendData({ name: "John", age: 30 });
-console.log(result1); // { 'access-token': 'qwerty', data: { name: 'John', age: 30 }, count: 1 }
+console.log(result1);
 
 const result2 = sendData({ name: "Jane", age: 25 });
-console.log(result2); // { 'access-token': 'qwerty', data: { name: 'Jane', age: 25 }, count: 2 }
+console.log(result2);
 
 const result3 = sendData({ name: "Doe", age: 40 });
-console.log(result3); // { 'access-token': 'qwerty', data: { name: 'Doe', age: 40 }, count: 3 }
+console.log(result3);
+
+////////////////////////////////////////////////////////////////////
+console.log("=== Завдання 2 ===");
+
+const obj = {
+  name: "John",
+  age: 30,
+  getData: function () {
+    console.log(`Person name is: ${this.name} and age ${this.age}`);
+  },
+};
+
+// Викликаємо метод getData, використовуючи об'єкт obj
+obj.getData();
+
+// Функція, яка завжди викликає getData з контекстом obj
+const alwaysGetData = obj.getData.bind(obj);
+
+// Приклад: викликаємо нову функцію декілька разів
+alwaysGetData();
+alwaysGetData();
